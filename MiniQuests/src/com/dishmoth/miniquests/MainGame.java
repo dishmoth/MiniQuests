a42d5df53f307a5e8cfb72444ad86ee1fbecb437
dishmoth <dishmoth@yahoo.co.uk>
2014-02-08 22:21:18 +0000

    Update version numbers.

--- MiniQuests/src/com/dishmoth/miniquests/MainGame.java
+++ MiniQuests/src/com/dishmoth/miniquests/MainGame.java
@@ -61,7 +61,7 @@ public class MainGame implements ApplicationListener {
 
     EnvBitsGdx envBits = new EnvBitsGdx();
     if ( Env.debugMode() ) {
-      envBits.debug("Mini Quests (v3.1.2, 2nd September 2013)");
+      envBits.debug("Mini Quests (v3.1.3, 8th February 2014)");
       envBits.debug("Contact: dishmoth@yahoo.co.uk, www.dishmoth.com");
       envBits.debug("");
  
