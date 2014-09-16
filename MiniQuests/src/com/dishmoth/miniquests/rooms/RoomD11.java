24e6add15c42c38f496636b35f4efdea7e99bef0
dishmoth <dishmoth@yahoo.co.uk>
2014-09-16 21:27:21 +0100

    Moved room D11 to D10.

--- MiniQuests/src/com/dishmoth/miniquests/rooms/RoomD11.java
+++ MiniQuests/src/com/dishmoth/miniquests/rooms/RoomD11.java
@@ -13,6 +13,7 @@ import com.dishmoth.miniquests.game.Env;
 import com.dishmoth.miniquests.game.Exit;
 import com.dishmoth.miniquests.game.Player;
 import com.dishmoth.miniquests.game.Room;
+import com.dishmoth.miniquests.game.Spikes;
 import com.dishmoth.miniquests.game.SpriteManager;
 import com.dishmoth.miniquests.game.StoryEvent;
 
@@ -22,7 +23,6 @@ public class RoomD11 extends Room {
   // unique identifier for this room
   public static final String NAME = "D11";
   
-  // main blocks for the room
   private static final String kBlocks[][] = { { "0000000000",
                                                 "0000000000",
                                                 "0000000000",
@@ -32,17 +32,63 @@ public class RoomD11 extends Room {
                                                 "0000000000",
                                                 "0000000000",
                                                 "0000000000",
-                                                "0000000000" } };
+                                                "0000000000" },
+                                                
+                                              { "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "1111111111",
+                                                "1111111111" },
+
+                                              { "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "1111111111",
+                                                "1111111111" },
+
+                                              { "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "11      11",
+                                                "1111111111",
+                                                "1111111111" } };
 
   // different block colours (corresponding to '0', '1', '2', etc)
-  private static final String kBlockColours[] = { "#U",
-                                                  "#U" }; 
+  private static final String kBlockColours[] = { "uO",
+                                                  "#l" }; 
   
   // details of exit/entry points for the room 
   private static final Exit kExits[]
-            = { new Exit(Env.UP,    5,0, "#U",0, -1, RoomD10.NAME, 1),
+            = { new Exit(Env.UP,    1,0, "#U",0, -1, RoomD10.NAME, 1),
                 new Exit(Env.RIGHT, 8,0, "#U",0, -1, RoomD12.NAME, 0) };
   
+  // times for things to happen
+  private static final int kSpikeDelay = 5,
+                           kOffDelay   = 20;
+  
+  // arrangement of spikes
+  private Spikes mSpikes[][];
+
+  // spike countdown
+  private int mTimer;
+
+  // which direction the spikes are going
+  private int mPhase;
+
   // constructor
   public RoomD11() {
 
@@ -65,9 +111,23 @@ public class RoomD11 extends Room {
   @Override
   public void createSprites(SpriteManager spriteManager) {
     
-    spriteManager.addSprite( new BlockArray(kBlocks, kBlockColours, 0,0,0) );
+    spriteManager.addSprite( new BlockArray(kBlocks, kBlockColours, 0,0,-6) );
     
     addBasicWalls(kExits, spriteManager);
+
+    mSpikes = new Spikes[6][3];
+    for ( int k = 0 ; k < mSpikes.length ; k++ ) {
+      mSpikes[k][0] = new Spikes(8,2+k,0, 2,1, false, "S0");
+      mSpikes[k][1] = new Spikes(2+k,0,0, 1,2, false, "S0");
+      mSpikes[k][2] = new Spikes(0,7-k,0, 2,1, false, "S0");
+      for ( int n = 0 ; n < mSpikes[k].length ; n++ ) {
+        if ( n > 0 ) mSpikes[k][n].setSilent(true);
+        spriteManager.addSprite(mSpikes[k][n]);
+      }
+    }
+
+    mTimer = 0;
+    mPhase = 0;
 
   } // Room.createSprites()
   
@@ -75,6 +135,8 @@ public class RoomD11 extends Room {
   @Override
   public void discardResources() {
 
+    mSpikes = null;
+    
   } // Room.discardResources()
   
   // update the room (events may be added or processed)
@@ -87,6 +149,22 @@ public class RoomD11 extends Room {
       storyEvents.add(new EventRoomChange(kExits[exitIndex].mDestination,
                                           kExits[exitIndex].mEntryPoint));
       return;
+    }
+
+    if ( --mTimer < 0 ) {
+      mTimer = kOffDelay + kSpikeDelay*mSpikes.length;
+      mPhase = (mPhase+1)%4;
+    }
+    
+    if ( mTimer % kSpikeDelay == 0 ) {
+      int num = mTimer/kSpikeDelay;
+      if ( num < mSpikes.length ) {
+        for ( int k = 0 ; k < mSpikes[num].length ; k++ ) {
+          boolean rev = (k==1) ? (mPhase==0||mPhase==3) : (mPhase<2);
+          int n = ( rev ? num : (mSpikes.length - 1 - num) );
+          mSpikes[n][k].trigger();
+        }
+      }
     }
 
   } // Room.advance()
