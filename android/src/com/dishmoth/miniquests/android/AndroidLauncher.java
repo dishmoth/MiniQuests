4c3d665e3e234b169a2c1a3990ec79c4fe7aeb02
dishmoth <dishmoth@yahoo.co.uk>
2015-03-15 13:53:53 +0000

    Moving to new libgdx file structure (ongoing...).  Adding android files.

---
+++ android/src/com/dishmoth/miniquests/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.dishmoth.miniquests.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dishmoth.miniquests.MiniQuestsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        //? cfg.useGL20 = false;
+		initialize(new MiniQuestsGame(), config);
+        //? getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	}
+}
